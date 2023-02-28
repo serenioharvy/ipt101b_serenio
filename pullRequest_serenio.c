c9fecac3064b50dc059f896ad3c83b69c62219b6
Shawn Kenneth Butron <butronkennethshawn@gmail.com>
2023-02-28 16:18:35 +0800

    Add pullRequest_serenio.c file

---
+++ pullRequest_serenio.c
@@ -0,0 +1,12 @@
+// Programmer 1. This part must be used for the main branch. 
+#include <stdio.h>
+
+int main() {
+    int num1=1, num2=2;
+    int result = num1 + num2;
+    
+    printf("The sum of %d and %d is %d\n", num1, num2, result);
+    
+    return 0;
+}
+
